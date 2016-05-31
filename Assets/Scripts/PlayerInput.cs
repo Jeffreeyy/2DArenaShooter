58cc757b0aafb4f46ad54752d9ed2ee1ed56b438
bennie12345 <bmuijldert@gmail.com>
2016-05-31 11:05:32 +0200

    fixed merge errors

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,26 +5,14 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour 
 {
     private PlayerMovement _movement;
-<<<<<<< HEAD
     private PlayerJetpack _playerJetpack;
-    private JetpackFuel _jetpackFuel;
-=======
-    private PlayerAbilities _abilities;
-    private PlayerPower _playerPower;
     private PlayerShoot _playerShoot;
->>>>>>> origin/master
 
     void Awake()
     {
         _movement = GetComponent<PlayerMovement>();
-<<<<<<< HEAD
         _playerJetpack = GetComponent<PlayerJetpack>();
-        _jetpackFuel = GetComponent<JetpackFuel>();
-=======
-        _abilities = GetComponent<PlayerAbilities>();
-        _playerPower = GetComponent<PlayerPower>();
         _playerShoot = GetComponent<PlayerShoot>();
->>>>>>> origin/master
     }
 
 	void Update () 
@@ -52,34 +40,19 @@ public class PlayerInput : MonoBehaviour
         //ABILITY 1
         if (Input.GetKey(KeyCode.Mouse0))
         {
-<<<<<<< HEAD
             _playerJetpack.ActivateJetpack();
-=======
             _movement.Jetpack();
->>>>>>> origin/master
         }
         else
             _playerJetpack.DeactivateJetpack();
         //ABILITY 2
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-<<<<<<< HEAD
             //_playerJetpack.Punch();
-=======
             if (_playerShoot.canShoot)
             {
                 StartCoroutine(_playerShoot.Shoot());
             }
-        }
-
-        if(!Input.anyKey && !Input.anyKeyDown)
-        {
-            _playerPower.IsStandingStill = true;
-        }
-        else
-        {
-            _playerPower.IsStandingStill = false;
->>>>>>> origin/master
         }
 	}
 }
