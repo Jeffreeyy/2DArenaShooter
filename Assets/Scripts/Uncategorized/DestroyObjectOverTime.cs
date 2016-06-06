53ee65ad3d29978c49e4f4a49509f6a1c0c64478
bennie12345 <bmuijldert@gmail.com>
2016-06-06 14:21:26 +0200

    used objectpool within destroy

---
+++ Assets/Scripts/Uncategorized/DestroyObjectOverTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObjectOverTime : MonoBehaviour {
+
+    [SerializeField]private float _destoryTime;
+
+	void Start () 
+    {
+        //Destroy(this.gameObject, _destoryTime);
+        ObjectPool.instance.PoolObject(this.gameObject);
+	}
+}
