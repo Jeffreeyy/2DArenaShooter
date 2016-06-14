5bc9d0986919c131ade5bf8505ae3029d6b05d21
bennie12345 <bmuijldert@gmail.com>
2016-06-14 12:38:44 +0200

    fixed playerinput

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -3,15 +3,31 @@ using System.Collections;
 
 public class PlayerInput : MonoBehaviour
 {
+    private PlayerGroundCollisionChecker2D _cc2d;
+    private PlayerMovement _playerMovement;
 	// Use this for initialization
 	void Start()
 	{
-		
+		_cc2d = GetComponent<PlayerGroundCollisionChecker2D>();
+        _playerMovement = GetComponent<PlayerMovement>();
 	}
 	
 	// Update() is called once per frame
 	void Update()
 	{
-		
+        if (Input.GetButtonDown("Xbox_LB_" + _playerMovement.PlayerID) && _cc2d.CanJump)
+        {
+            _playerMovement.Jump();
+        }
+
+        if (Input.GetAxis("Xbox_RT_" + _playerMovement.PlayerID) > 0.25f)
+        {
+            _playerMovement.Shoot();
+        }
+
+        if (Input.GetAxis("Xbox_LT_" + _playerMovement.PlayerID) > 0.25f)
+        {
+            _playerMovement.Jetpack();
+        }
 	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private int _playerID;
     public int PlayerID
     {
+        get { return _playerID; }
         set { _playerID = value; }
     }
     private float _speed = 10f;
     private Rigidbody2D _rb2d;
-    private PlayerGroundCollisionChecker2D _cc2d;
+    
     private PlayerShoot _shoot;
     private AudioSource _jumpSound;
 
@@ -18,28 +19,29 @@ public class PlayerMovement : MonoBehaviour
     {
         _shoot = GetComponent<PlayerShoot>();
         _rb2d = GetComponent<Rigidbody2D>();
-        _cc2d = GetComponent<PlayerGroundCollisionChecker2D>();
+        
         _jumpSound = GetComponent<AudioSource>();
     }
     void Update()
     {
         float x = Input.GetAxis("Xbox_JoystickLeft_X_" + _playerID);
         _rb2d.velocity = new Vector2(x * _speed, _rb2d.velocity.y);
-        if (Input.GetButtonDown("Xbox_LB_" + _playerID) && _cc2d.CanJump)
-        {
-            _rb2d.velocity = new Vector2(0, 25f);
-            _jumpSound.pitch = Random.Range(.75f, 1.25f);
-            _jumpSound.Play();
-        }
+    }
 
-        if (Input.GetAxis("Xbox_RT_" + _playerID) > 0.25f || Input.GetKey(KeyCode.Mouse0))
-        {
-                StartCoroutine(_shoot.Shoot());
-        }
+    public void Jump()
+    {
+        _rb2d.velocity = new Vector2(0, 25f);
+        _jumpSound.pitch = Random.Range(.75f, 1.25f);
+        _jumpSound.Play();
+    }
 
-        if (Input.GetAxis("Xbox_LT_" + _playerID) > 0.25f)
-        {
-            _rb2d.AddForce(Vector2.up * Input.GetAxis("Xbox_LT_" + _playerID) * 100, ForceMode2D.Force);
-        }
+    public void Shoot()
+    {
+        StartCoroutine(_shoot.Shoot());
+    }
+
+    public void Jetpack()
+    {
+        _rb2d.AddForce(Vector2.up * Input.GetAxis("Xbox_LT_" + _playerID) * 100, ForceMode2D.Force);
     }
 }
