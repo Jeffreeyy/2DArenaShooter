62b99691429628fc790e8e9d8d12a19f33b2ac69
bennie12345 <bmuijldert@gmail.com>
2016-06-14 16:31:31 +0200

    added in health and fuel bar for player

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,11 +5,13 @@ public class PlayerInput : MonoBehaviour
 {
     private PlayerGroundCollisionChecker2D _cc2d;
     private PlayerMovement _playerMovement;
+    private JetpackFuel _jetpackFuel;
 	// Use this for initialization
 	void Start()
 	{
 		_cc2d = GetComponent<PlayerGroundCollisionChecker2D>();
         _playerMovement = GetComponent<PlayerMovement>();
+        _jetpackFuel = GetComponent<JetpackFuel>();
 	}
 	
 	// Update() is called once per frame
@@ -25,9 +27,16 @@ public class PlayerInput : MonoBehaviour
             _playerMovement.Shoot();
         }
 
-        if (Input.GetAxis("Xbox_LT_" + _playerMovement.PlayerID) > 0.25f)
+        if (Input.GetAxis("Xbox_LT_" + _playerMovement.PlayerID) > 0.25f && _jetpackFuel.Fuel > 0 )
         {
             _playerMovement.Jetpack();
+            _jetpackFuel.IsUsingJetpack = true;
+            _jetpackFuel.IsRecharging = false;
+        }
+        else 
+        {
+            _jetpackFuel.IsDrainingFuel = false;
+            _jetpackFuel.IsUsingJetpack = false;
         }
 	}
 }
