f98f85200327635e7531ea35e5534a345f89254e
bennie12345 <bmuijldert@gmail.com>
2016-06-16 21:25:07 +0200

    furthered UI navigation

--- Assets/Scripts/Data/SceneNames.cs
+++ Assets/Scripts/Data/SceneNames.cs
@@ -7,4 +7,5 @@ public class SceneNames : MonoBehaviour
     public static readonly string INSTRUCTIONSSCENE = "instructions";
     public static readonly string VICTORYSCENE = "victoryscreen";
     public static readonly string CHARACTERSELECTIONSCENE = "characterselectionscene";
+    public static readonly string MENUSCENE = "menu";
 }
