cf6fb90b439eb2d7c8fee7383618cef5ce62d485
Jeffreeyy <17844@ma-web.nl>
2016-06-17 18:12:02 +0200

    fixed teleporter

--- Assets/Scripts/Uncategorized/Teleporter.cs
+++ Assets/Scripts/Uncategorized/Teleporter.cs
@@ -3,25 +3,24 @@ using System.Collections;
 
 public class Teleporter : MonoBehaviour 
 {
-    [SerializeField]private float _offset;
-    private GameObject _portal1;
-    private GameObject _portal2;
-	
+    private Transform _portal1;
+    private Transform _portal2;
+
     void Start()
     {
-        _portal1 = GameObject.Find("Portal1").gameObject;
-        _portal2 = GameObject.Find("Portal2").gameObject;
+        _portal1 = GameObject.Find("Portal1").GetComponent<Transform>();
+        _portal2 = GameObject.Find("Portal2").GetComponent<Transform>();
     }
 
-	void Update () 
+    void Update()
     {
-	    if(this.transform.position.x > _portal2.transform.position.x - _offset)
+        if(this.transform.position.x < _portal1.position.x)
         {
-            this.transform.position = new Vector3(_portal1.transform.position.x, this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(_portal2.transform.position.x, transform.position.y,transform.position.z);
         }
-        if(this.transform.position.x < _portal1.transform.position.x + _offset)
+        if (this.transform.position.x > _portal2.position.x)
         {
-            this.transform.position = new Vector3(_portal2.transform.position.x, this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(_portal1.transform.position.x, transform.position.y, transform.position.z);
         }
-	}
+    }
 }
